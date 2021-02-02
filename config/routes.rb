eb70da6da48965c30ed01e2e2d129ca345ec91fc
mikik0 <usapon_love1222@yahoo.co.jp>
2021-02-02 20:54:57 +0900

    root変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
   resources :tasks
-  root  'users/sessions#create'
-  # devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root  'tasks#index'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
+    :sessions => 'users/sessions'
   }
-  get 'contact', to: 'contacts#index'             #入力画面
-  post 'contact/confirm' => 'contacts#confirm'   # 確認画面
-  post 'contact/complete' => 'contacts#complete' # 送信完了画面
 
   devise_scope :user do
     get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy" 
+    get "sign_out", :to => "users/sessions#destroy"
   end
+
 end
