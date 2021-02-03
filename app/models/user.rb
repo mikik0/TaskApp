1135086e09ebe54128235b4e71143b14a1b6a3ec
mikik0 <mikiko.hashino@super-studio.jp>
2021-02-03 13:53:32 +0900

    ユーザーネームを変更できるようにしたなど

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+    devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable, :confirmable
+         
+    has_many :tasks, dependent: :destroy
 end
