389855c9b469d34ad5b438b82257385b611d54dc
mikik0 <mikiko.hashino@super-studio.jp>
2021-02-09 19:42:42 +0900

    mailer変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+   user_name: ENV['SENDGRID_USERNAME'], #環境変数化
+   password: ENV['SENDGRID_PASSWORD'], #環境変数化
+   domain: "heroku.com",
+   address: "smtp.sendgrid.net",
+   port: 587,
+   authentication: :plain,
+   enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: 'boiling-temple-42102.herokuapp.com' }
+
 end
